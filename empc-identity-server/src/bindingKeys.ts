8cb88c0ecced13dbcca02b5c5d7560047d257d1b
Napihuphup <programminghup@gmail.com>
2020-04-20 22:15:36 +0800

    Update bindingKeys.ts

--- empc-identity-server/src/bindingKeys.ts
+++ empc-identity-server/src/bindingKeys.ts
@@ -43,8 +43,8 @@ const setKeyPath = () => {
       break;
     }
     case 'production': {
-      auth_public_path = "../../../../empcord-keys/empcord.oauth.public.key"
-      auth_private_path = "../../../../empcord-keys/empcord.oauth.private.key"
+      auth_public_path = "../../../../empcord-keys/empcord.auth.public.key"
+      auth_private_path = "../../../../empcord-keys/empcord.auth.private.key"
       api_public_path = "../../../../empcord-keys/empcord.api.public.key"
       api_private_path = "../../../../empcord-keys/empcord.api.private.key"
       break;
